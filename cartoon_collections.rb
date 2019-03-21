b7d49c648a9130876357c63cf29b3ecaed3a944c
Nicholas Rodriguez <niko876@hotmail.com>
2019-03-21 02:20:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(dwarves)
-  dwarves.each_with_index(0) {|dwarf, index| puts "#{dwarf[index]}"}
+  dwarves.each_with_index {|dwarf, index| puts "#{dwarf[index]}"}
 end
 
 def summon_captain_planet# code an argument here
